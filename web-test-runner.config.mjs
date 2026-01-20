2c0b7825f9c773f9b87d23ed92d623616824088a
Meghea Iulian <iulian@plumelo.com>
2026-01-20 11:49:35 +0200

    fix: adjust test thresholds

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,4 +3,13 @@ import cfg from '@neovici/cfg/web/test-runner.mjs';
 export default {
 	...cfg,
 	files: 'src/**/*.test.(j|t)s',
+	coverageConfig: {
+		...cfg.coverageConfig,
+		threshold: {
+			statements: 65,
+			branches: 70,
+			functions: 40,
+			lines: 65,
+		},
+	},
 };
